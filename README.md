d0b3af42dd22cdb3bf91fa39c2649c801ca09028
Isaac Mina <isaacmina93@gmail.com>
2021-07-24 12:27:57 -0700

    change calculator

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 ### Overview
 Explain the basic idea of the project
-
+The purpose of the project is to design a cash register program that will take 
+input from the user to calculate the amount of change owed as well as the 
+exact number of quarters, dimes, nickels, and pennies.
 
 ### How to use
-Provides the user instructions on how to run your project
+To run the program, provide the cost of the purchase and then provide the amount
+of money paid.
 
 ### Technologies
 
@@ -14,3 +17,5 @@ Provides the user instructions on how to run your project
 
 ###
 Main takeaways from work on the project
+My biggest takeaway is to work on your code bit by bit and to comment it so you
+don't lose track of what you were doing
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -10,28 +10,43 @@ public class MakeChange {
 		System.out.println("Enter amount tendered: ");
 		double numpaid = kb.nextDouble();
 
-		// Returns an error for not enough
+// Returns an error for not enough money paid
 		if (numcost > numpaid) {
 			System.out.println("Insufficient funds. ");
 		}
-		// 
+// returns message for exact change 
 		if (numcost == numpaid) {
 			System.out.println("Payment approved! ");
 		}
 		if (numcost < numpaid) {
-			double numDifference = numpaid - numcost;
-			CalChange();
-			System.out.println("Amount owed: $" + numDifference);
-		}
+// set values for coins to calculate change owed
+			double quarter = 0.25;
+			double dime = 0.10;
+		      double nickel = 0.05;
+		      double penny = 0.01;
+		      
+// round changeDue to 2 decimal places
+		      double numChange = ( (double)((int) Math.round((numpaid - numcost)*100)) / 100.0 );
+// determine the value each coin denomination needed for change
+		      double modQuarters = ( (double)((int) Math.round((numChange % quarter)*100)) / 100.0 );
+		      double modDimes = ( (double)((int) Math.round((modQuarters % dime)*100)) / 100.0 );
+		      double modNickels = ( (double)((int) Math.round((modQuarters % nickel)*100)) / 100.0 );
+		      double modPennies = ( (double)((int) Math.round((modQuarters % penny)*100)) / 100.0 );
+		      
+// determine the amount of of coins for each coin denomination 
+		      int numQuarters = (int)((numChange - modQuarters) / (quarter));
+		      int numDimes = (int)((modQuarters - modDimes) / (dime));
+		      int numNickels = (int)((modDimes - modNickels) / (nickel));
+		      int numPennies = (int)((modNickels - modPennies) / (penny));			
+
+// Return message with exact coins needed
+		    System.out.println("Amount owed: $" + numChange);
+			System.out.println("Number of quarters owed: " + numQuarters);
+		    System.out.println("Number of dimes owed: " + numDimes);
+		    System.out.println("Number of nickels owed: " + numNickels);
+		    System.out.println("Number of pennies owed: " + numPennies);		}
 
 	}
 
 	
-	private static void CalChange() {
-	// define a variable for coin values
-		double quarter = 0.25;
-		double dime = 0.10;
-		double nickel = 0.05;
-		double penny = 0.01;
-		}
 }
