e5b7125a5246b3b6dc7efc4714889fb023c9e438
Isaac Mina <isaacmina93@gmail.com>
2021-07-25 23:30:02 -0700

    fixed dollar amount

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -20,6 +20,8 @@ public class MakeChange {
 		}
 		if (numcost < numpaid) {
 // set values for coins to calculate change owed
+			double tenDollar = 10;
+			double dollar = 1;
 			double quarter = 0.25;
 			double dime = 0.10;
 			double nickel = 0.05;
@@ -27,20 +29,27 @@ public class MakeChange {
 
 // round changeDue to 2 decimal places
 			double numChange = ((double) ((int) Math.round((numpaid - numcost) * 100)) / 100.0);
-// determine the value each coin denomination needed for change
-			double modQuarters = ((double) ((int) Math.round((numChange % quarter) * 100)) / 100.0);
+// determine the value for each coin denomination needed for change
+			
+			double modTenDollars = ((double) ((int) Math.round((numChange % tenDollar) * 100)) / 100.0);
+			double modDollars = ((double) ((int) Math.round((modTenDollars % dollar) * 100)) / 100.0);
+			double modQuarters = ((double) ((int) Math.round((modDollars % quarter) * 100)) / 100.0);
 			double modDimes = ((double) ((int) Math.round((modQuarters % dime) * 100)) / 100.0);
 			double modNickels = ((double) ((int) Math.round((modQuarters % nickel) * 100)) / 100.0);
 			double modPennies = ((double) ((int) Math.round((modQuarters % penny) * 100)) / 100.0);
 
 // determine the amount of of coins for each coin denomination 
-			int numQuarters = (int) ((numChange - modQuarters) / (quarter));
+			int numTenDollars = (int) ((numChange - modTenDollars) / (tenDollar));
+			int numDollars = (int) ((modTenDollars - modDollars) / (dollar));
+			int numQuarters = (int) ((modDollars - modQuarters) / (quarter));
 			int numDimes = (int) ((modQuarters - modDimes) / (dime));
 			int numNickels = (int) ((modDimes - modNickels) / (nickel));
 			int numPennies = (int) ((modNickels - modPennies) / (penny));
 
 // Return message with exact coins needed
 			System.out.println("Amount owed: $" + numChange);
+			System.out.println("Number of ten dollar bills owed: " + numTenDollars);
+			System.out.println("Number of dollar bills owed: " + numDollars);
 			System.out.println("Number of quarters owed: " + numQuarters);
 			System.out.println("Number of dimes owed: " + numDimes);
 			System.out.println("Number of nickels owed: " + numNickels);
