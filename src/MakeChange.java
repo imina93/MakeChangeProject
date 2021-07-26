a22aa276e3516f1e42b629ac888c40b700b27d34
Isaac Mina <isaacmina93@gmail.com>
2021-07-25 23:14:03 -0700

    updated change calc

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -22,31 +22,31 @@ public class MakeChange {
 // set values for coins to calculate change owed
 			double quarter = 0.25;
 			double dime = 0.10;
-		      double nickel = 0.05;
-		      double penny = 0.01;
-		      
+			double nickel = 0.05;
+			double penny = 0.01;
+
 // round changeDue to 2 decimal places
-		      double numChange = ( (double)((int) Math.round((numpaid - numcost)*100)) / 100.0 );
+			double numChange = ((double) ((int) Math.round((numpaid - numcost) * 100)) / 100.0);
 // determine the value each coin denomination needed for change
-		      double modQuarters = ( (double)((int) Math.round((numChange % quarter)*100)) / 100.0 );
-		      double modDimes = ( (double)((int) Math.round((modQuarters % dime)*100)) / 100.0 );
-		      double modNickels = ( (double)((int) Math.round((modQuarters % nickel)*100)) / 100.0 );
-		      double modPennies = ( (double)((int) Math.round((modQuarters % penny)*100)) / 100.0 );
-		      
+			double modQuarters = ((double) ((int) Math.round((numChange % quarter) * 100)) / 100.0);
+			double modDimes = ((double) ((int) Math.round((modQuarters % dime) * 100)) / 100.0);
+			double modNickels = ((double) ((int) Math.round((modQuarters % nickel) * 100)) / 100.0);
+			double modPennies = ((double) ((int) Math.round((modQuarters % penny) * 100)) / 100.0);
+
 // determine the amount of of coins for each coin denomination 
-		      int numQuarters = (int)((numChange - modQuarters) / (quarter));
-		      int numDimes = (int)((modQuarters - modDimes) / (dime));
-		      int numNickels = (int)((modDimes - modNickels) / (nickel));
-		      int numPennies = (int)((modNickels - modPennies) / (penny));			
+			int numQuarters = (int) ((numChange - modQuarters) / (quarter));
+			int numDimes = (int) ((modQuarters - modDimes) / (dime));
+			int numNickels = (int) ((modDimes - modNickels) / (nickel));
+			int numPennies = (int) ((modNickels - modPennies) / (penny));
 
 // Return message with exact coins needed
-		    System.out.println("Amount owed: $" + numChange);
+			System.out.println("Amount owed: $" + numChange);
 			System.out.println("Number of quarters owed: " + numQuarters);
-		    System.out.println("Number of dimes owed: " + numDimes);
-		    System.out.println("Number of nickels owed: " + numNickels);
-		    System.out.println("Number of pennies owed: " + numPennies);		}
+			System.out.println("Number of dimes owed: " + numDimes);
+			System.out.println("Number of nickels owed: " + numNickels);
+			System.out.println("Number of pennies owed: " + numPennies);
+		}
 
 	}
 
-	
 }
